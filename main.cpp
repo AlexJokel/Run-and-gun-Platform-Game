1c48f91c3febfcd58ff02a82bcaec5048ca54a55
Alexey Bakevich <bakevich@list.ru>
2019-02-26 11:30:09 +0300

    Added window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <QApplication>
 
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+#include <QGraphicsView>
+
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+
+  auto scene = new QGraphicsScene();
+  auto view = new QGraphicsView(scene);
+  view->show();
 
   return a.exec();
 }
