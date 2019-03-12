af7ad7c1a03e754a304bcc4466466e91b7513a05
Alexey Bakevich <bakevich@list.ru>
2019-03-12 10:38:10 +0300

    Created falling player

--- player.cpp
+++ player.cpp
@@ -1,23 +1,49 @@
 #include "player.h"
 
-#include <QKeyEvent>
-#include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QDebug>
 
-Player::Player(QGraphicsItem* parent) : QGraphicsRectItem(parent)
-{
-  setRect(0, 0, 100, 100);
+Player::Player(b2World* world, QGraphicsItem* parent)
+    : QGraphicsRectItem(parent) {
+  body_def_ = new b2BodyDef;
+  body_def_->type = b2_dynamicBody;
+  body_def_->position.Set(3, 3);
+  body_shape_ = new b2PolygonShape;
+  body_shape_->SetAsBox(0.5f, 0.5f);
+  body_fixture_def_ = new b2FixtureDef;
+  body_fixture_def_->shape = body_shape_;
+  body_fixture_def_->density = 1;
+
+  body_ = world->CreateBody(body_def_);
+  body_->CreateFixture(body_fixture_def_);
+
+  Draw();
 }
 
 void Player::advance(int phase)
 {
   if (phase == 0) return;
-  move();
+  Draw();
   scene()->views().front()->centerOn(this);
 }
 
 Scene* Player::Scene() const {
   return dynamic_cast<class Scene*>(scene());
+}
+
+Player::~Player() {
+  body_->GetWorld()->DestroyBody(body_);
+}
+
+void Player::Draw() {
+  float32 half_width = qAbs(body_shape_->m_vertices[0].x);
+  float32 half_height = qAbs(body_shape_->m_vertices[0].y);
+  float32 x = body_->GetPosition().x;
+  float32 y = body_->GetPosition().y;
+  setRect(Scene::MetersToPixels(x - half_width),
+          Scene::MetersToPixels(y - half_height),
+          Scene::MetersToPixels(2 * half_width),
+          Scene::MetersToPixels(2 * half_height));
 }
 
 void Player::move()
--- player.h
+++ player.h
@@ -4,18 +4,28 @@
 #include <QGraphicsRectItem>
 #include <QMap>
 
+#include <Box2D/Box2D.h>
+
 #include "scene.h"
 
 class Player : public QGraphicsRectItem
 {
 public:
-  Player(QGraphicsItem* = nullptr);
+  Player(b2World*, QGraphicsItem* = nullptr);
 
   void advance(int) override;
 
   Scene* Scene() const;
 
+  ~Player() override;
+
 private:
+  b2Body* body_;
+  b2BodyDef* body_def_;
+  b2FixtureDef* body_fixture_def_;
+  b2PolygonShape* body_shape_;
+
+  void Draw();
   void move();
 };
 
