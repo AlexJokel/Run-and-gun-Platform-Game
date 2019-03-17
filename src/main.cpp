829f2c3d57aee61c3afeef593015c093bc7ced72
Alexey Bakevich <bakevich@list.ru>
2019-03-17 18:14:32 +0300

    Added some colors

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,17 @@ int main(int argc, char *argv[]) {
   view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
+  /// Draw dot grid
+  for (size_t x = 0; x < scene->width(); x += 100) {
+    for (size_t y = 0; y < scene->height(); y += 100) {
+      scene->addItem(new QGraphicsRectItem(x, y, 1, 1));
+    }
+  }
+
   /// Player initialization
   auto player = new Player(scene, 3, 3);
+  player->setBrush(Qt::darkGreen);
+  player->setPen(Qt::NoPen);
 
   /// Create floor
   auto floor = new Ground(scene,
@@ -35,6 +44,8 @@ int main(int argc, char *argv[]) {
                           Scene::PixelsToMeters(scene->height()) - 1,
                           Scene::PixelsToMeters(scene->width()),
                           1);
+  floor->setBrush(Qt::darkGray);
+  floor->setPen(Qt::NoPen);
 
   /// Create walls
   auto left_wall = new Ground(scene,
@@ -42,18 +53,15 @@ int main(int argc, char *argv[]) {
                               0,
                               1,
                               Scene::PixelsToMeters(scene->height()));
+  left_wall->setBrush(Qt::darkGray);
+  left_wall->setPen(Qt::NoPen);
   auto right_wall = new Ground(scene,
                                Scene::PixelsToMeters(scene->width()) - 1,
                                0,
                                1,
                                Scene::PixelsToMeters(scene->height()));
-
-  /// Draw dot grid
-  for (size_t x = 0; x < scene->width(); x += 100) {
-    for (size_t y = 0; y < scene->height(); y += 100) {
-      scene->addItem(new QGraphicsRectItem(x, y, 1, 1));
-    }
-  }
+  right_wall->setBrush(Qt::darkGray);
+  right_wall->setPen(Qt::NoPen);
 
   view->show();
 
