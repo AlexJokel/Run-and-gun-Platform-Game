4aef1126ece2d4de95ac0bb1fe052c083f045fba
Alexey Bakevich <bakevich@list.ru>
2019-04-01 18:54:37 +0300

    Fixed warning in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
 
-  /// Scene & view initialization
-  auto game = new Game();
+  /// Game start
+  new Game();
 
   return a.exec();
 }
