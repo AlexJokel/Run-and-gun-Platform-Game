d95f56c31555e6dbdfd378df00e9a229e4e15f7d
Alexey Bakevich <bakevich@list.ru>
2019-04-01 22:09:55 +0300

    Changed contact processing

--- src/contactlistener.cpp
+++ src/contactlistener.cpp
@@ -6,23 +6,51 @@
 #include "enemy.h"
 
 void ContactListener::BeginContact(b2Contact* contact) {
-  auto player_enemy = CheckContactedTypes<Player, Enemy>(contact);
-  if (player_enemy.first != nullptr) {
-    player_enemy.first->Level()->RemoveObject(player_enemy.first);
-    player_enemy.second->Level()->RemoveObject(player_enemy.second);
+  QPair<Object*, Object*> contacted_objects = {
+    static_cast<Object*>(contact->GetFixtureA()->GetBody()->GetUserData()),
+    static_cast<Object*>(contact->GetFixtureB()->GetBody()->GetUserData())};
+
+  /// Check for arrow-anything collision
+  if (CheckContactedTypes(&contacted_objects, ObjectType::kArrow)) {
+    contacted_objects.first->Level()->RemoveObject(contacted_objects.first);
+    if (contacted_objects.second->Type() == ObjectType::kEnemy) {
+      contacted_objects.second->Level()->RemoveObject(contacted_objects.second);
+    }
     return;
   }
 
-  auto arrow_enemy = CheckContactedTypes<Arrow, Enemy>(contact);
-  if (arrow_enemy.first != nullptr) {
-    arrow_enemy.first->Level()->RemoveObject(arrow_enemy.first);
-    arrow_enemy.second->Level()->RemoveObject(arrow_enemy.second);
+  /// Check for player-enemy collision
+  if (CheckContactedTypes(&contacted_objects,
+                          ObjectType::kPlayer, ObjectType::kEnemy)) {
+    contacted_objects.first->Level()->RemoveObject(contacted_objects.first);
+    contacted_objects.second->Level()->RemoveObject(contacted_objects.second);
     return;
   }
+}
 
-  auto arrow = CheckContactedTypes<Arrow>(contact);
-  if (arrow != nullptr) {
-    arrow->Level()->RemoveObject(arrow);
-    return;
+bool ContactListener::CheckContactedTypes(QPair<Object*, Object*>* objects,
+                                          ObjectType type1,
+                                          ObjectType type2) const {
+  if ((objects->first->Type() == type1) &&
+      (objects->second->Type() == type2)) {
+    return true;
   }
+  if ((objects->first->Type() == type2) &&
+      (objects->second->Type() == type1)) {
+    qSwap(objects->first, objects->second);
+    return true;
+  }
+  return false;
+}
+
+bool ContactListener::CheckContactedTypes(QPair<Object*, Object*>* objects,
+                                          ObjectType type) const {
+  if (objects->first->Type() == type) {
+    return true;
+  }
+  if (objects->second->Type() == type) {
+    qSwap(objects->first, objects->second);
+    return true;
+  }
+  return false;
 }
