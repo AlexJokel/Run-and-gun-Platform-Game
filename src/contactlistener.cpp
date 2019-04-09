ef7609b784fe89c265660fcbeb66b50759c0009d
Alexey Bakevich <bakevich@list.ru>
2019-04-09 21:38:56 +0300

    Changed contact processing (even better now)

--- src/contactlistener.cpp
+++ src/contactlistener.cpp
@@ -5,26 +5,34 @@
 #include "level.h"
 #include "enemy.h"
 
+#include <QMap>
+
 void ContactListener::BeginContact(b2Contact* contact) {
   QPair<Object*, Object*> contacted_objects = {
     static_cast<Object*>(contact->GetFixtureA()->GetBody()->GetUserData()),
     static_cast<Object*>(contact->GetFixtureB()->GetBody()->GetUserData())};
 
-  /// Check for arrow-anything collision
-  if (CheckContactedTypes(&contacted_objects, ObjectType::kArrow)) {
-    contacted_objects.first->Level()->RemoveObject(contacted_objects.first);
-    if (contacted_objects.second->Type() == ObjectType::kEnemy) {
-      contacted_objects.second->Level()->RemoveObject(contacted_objects.second);
+  /// Objects of these types get deleted on ANY contact
+  const QVector<ObjectType> deleted_on_any_contact_types = {
+    ObjectType::kArrow, ObjectType::kBullet
+  };
+  for (const auto& type : deleted_on_any_contact_types) {
+    if (CheckContactedTypes(&contacted_objects, type)) {
+      contacted_objects.first->Level()->RemoveObject(contacted_objects.first);
     }
-    return;
   }
 
-  /// Check for player-enemy collision
-  if (CheckContactedTypes(&contacted_objects,
-                          ObjectType::kPlayer, ObjectType::kEnemy)) {
-    contacted_objects.first->Level()->RemoveObject(contacted_objects.first);
-    contacted_objects.second->Level()->RemoveObject(contacted_objects.second);
-    return;
+  /// Objects of key types get deleted on contact with a type in a value vector
+  const QMap<ObjectType, QVector<ObjectType>> deleted_on_contact_types = {
+    {ObjectType::kPlayer, {ObjectType::kBullet, ObjectType::kEnemy}},
+    {ObjectType::kEnemy, {ObjectType::kArrow, ObjectType::kPlayer}},
+  };
+  for (const auto& key_type : deleted_on_contact_types.keys()) {
+    for (const auto& value_type : deleted_on_contact_types.value(key_type)) {
+      if (CheckContactedTypes(&contacted_objects, key_type, value_type)) {
+        contacted_objects.first->Level()->RemoveObject(contacted_objects.first);
+      }
+    }
   }
 }
 
